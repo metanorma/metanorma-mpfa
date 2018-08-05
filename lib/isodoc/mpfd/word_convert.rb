d1fd835d298a6afa331344b844f15d8f4c49b1ef
Nick Nicholas <opoudjis@optusnet.com.au>
2018-08-05 17:29:01 +1000

    init

--- lib/isodoc/mpfd/word_convert.rb
+++ lib/isodoc/mpfd/word_convert.rb
@@ -151,6 +151,18 @@ module IsoDoc
         docxml
       end
 
+      def i18n_init(lang, script)
+        super
+        y = if lang == "en"
+              YAML.load_file(File.join(File.dirname(__FILE__), "i18n-en.yaml"))
+            elsif lang == "zh" && script == "Hans"
+              YAML.load_file(File.join(File.dirname(__FILE__),
+                                       "i18n-zh-Hans.yaml"))
+            else
+              YAML.load_file(File.join(File.dirname(__FILE__), "i18n-zh-Hans.yaml"))
+            end
+        @labels = @labels.merge(y)
+      end
     end
   end
 end
