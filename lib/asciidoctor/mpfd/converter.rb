c2ecbe3f9a264b0284d1f6d8133659b05bbb833c
Nick Nicholas <opoudjis@optusnet.com.au>
2019-01-28 01:02:06 +0000

    better handling of CR in XML; https://github.com/riboseinc/metanorma-standoc/issues/43

--- lib/asciidoctor/mpfd/converter.rb
+++ lib/asciidoctor/mpfd/converter.rb
@@ -81,7 +81,7 @@ module Asciidoctor
         result << noko { |ixml| front node, ixml }
         result << noko { |ixml| middle node, ixml }
         result << "</mpfd-standard>"
-        result = textcleanup(result.flatten * "\n")
+        result = textcleanup(result)
         ret1 = cleanup(Nokogiri::XML(result))
         validate(ret1)
         ret1.root.add_namespace(nil, MPFD_NAMESPACE)
