d57c8820291e8c37707688f3181c97fc66096219
Nick Nicholas <opoudjis@gmail.com>
2020-11-09 19:18:14 +1100

    Annex xref l10n fix: metanorma/metanorma-iso#433

--- lib/isodoc/mpfa/xref.rb
+++ lib/isodoc/mpfa/xref.rb
@@ -76,7 +76,7 @@ module IsoDoc
 
       def annex_names(clause, num)
         @anchors[clause["id"]] = { label: annex_name_lbl(clause, num), value: num,
-                                   xref: "#{@labels['annex']} #{num}", level: 1 }
+                                   xref: l10n("#{@labels['annex']} #{num}"), level: 1 }
         if a = single_annex_special_section(clause)
           annex_names1(a, "#{num}", 1)
         else
@@ -91,7 +91,7 @@ module IsoDoc
 
       def annex_names1(clause, num, level)
         clause["container"] or @anchors[clause["id"]] =
-          { label: num, xref: "#{@labels['annex']} #{num}", level: level }
+          { label: num, xref: l10n("#{@labels['annex']} #{num}"), level: level }
         i = 0
         clause.xpath(ns("./clause | ./references")).each do |c|
           i = annex_naming(c, num, level, i)
