4594a8167d1c9c1ec4dba1f0a9c0fcfec43aa57a
Alexande B <abobrikovich@gmail.com>
2021-02-09 19:21:06 +0300

    Update .rubocop.yml + fix rubocop issues in test + generate test files in temp directory

--- spec/metanorma/processor_spec.rb
+++ spec/metanorma/processor_spec.rb
@@ -1,15 +1,19 @@
 require "spec_helper"
 require "metanorma"
 require "fileutils"
+require "pathname"
 
 RSpec.describe Metanorma::MPFA::Processor do
-
   registry = Metanorma::Registry.instance
   registry.register(Metanorma::MPFA::Processor)
 
-  let(:processor) {
-    registry.find_processor(:mpfa)
-  }
+  let(:processor) { registry.find_processor(:mpfa) }
+
+  around do |example|
+    Dir.mktmpdir("rspec-") do |dir|
+      Dir.chdir(dir) { example.run }
+    end
+  end
 
   it "registers against metanorma" do
     expect(processor).not_to be nil
@@ -17,7 +21,7 @@ RSpec.describe Metanorma::MPFA::Processor do
 
   it "registers output formats against metanorma" do
     expect(processor.output_formats.sort.to_s).to be_equivalent_to <<~"OUTPUT"
-    [[:doc, "doc"], [:html, "html"], [:pdf, "pdf"], [:presentation, "presentation.xml"], [:rxl, "rxl"], [:xml, "xml"]]
+      [[:doc, "doc"], [:html, "html"], [:pdf, "pdf"], [:presentation, "presentation.xml"], [:rxl, "rxl"], [:xml, "xml"]]
     OUTPUT
   end
 
@@ -27,28 +31,27 @@ RSpec.describe Metanorma::MPFA::Processor do
 
   it "generates IsoDoc XML from a blank document" do
     input = <<~"INPUT"
-    #{ASCIIDOC_BLANK_HDR}
+      #{ASCIIDOC_BLANK_HDR}
     INPUT
 
     output = <<~"OUTPUT"
-    #{BLANK_HDR}
-<sections/>
-</mpfd-standard>
+      #{BLANK_HDR}
+      <sections/>
+      </mpfd-standard>
     OUTPUT
 
     expect(xmlpp(processor.input_to_isodoc(input, nil))).to be_equivalent_to xmlpp(output)
   end
 
   it "generates XML from IsoDoc XML" do
-    FileUtils.rm_f "test.xml"
     input = <<~"INPUT"
-    <mpfd-standard xmlns="http://riboseinc.com/isoxml">
-      <sections>
-        <clause id="H" obligation="normative"><title>Clause</title>
-          <p>Text</p>
-        </clause>
-      </sections>
-    </mpfd-standard>
+      <mpfd-standard xmlns="http://riboseinc.com/isoxml">
+        <sections>
+          <clause id="H" obligation="normative"><title>Clause</title>
+            <p>Text</p>
+          </clause>
+        </sections>
+      </mpfd-standard>
     INPUT
 
     processor.output(input, "test.xml", "test.xml", :xml)
@@ -56,65 +59,63 @@ RSpec.describe Metanorma::MPFA::Processor do
   end
 
   it "generates HTML from IsoDoc XML" do
-    FileUtils.rm_f "test.xml"
-    FileUtils.rm_f "test.html"
     input = <<~"INPUT"
-    <mpfd-standard xmlns="http://riboseinc.com/isoxml">
-      <sections>
-        <clause id="H" obligation="normative"><title>1.<tab/>Clause</title>
-          <p>Text</p>
-        </clause>
-      </sections>
-    </mpfd-standard>
+      <mpfd-standard xmlns="http://riboseinc.com/isoxml">
+        <sections>
+          <clause id="H" obligation="normative"><title>1.<tab/>Clause</title>
+            <p>Text</p>
+          </clause>
+        </sections>
+      </mpfd-standard>
     INPUT
 
     output = <<~"OUTPUT"
-    <main class="main-section"><button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
-      <p class="zzSTDTitle1"></p>
-      <div id="H">
-        <h1 id="toc0">1.&#xA0; Clause</h1>
-        <p>Text</p>
-      </div>
-    </main>
+      <main class="main-section"><button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+        <p class="zzSTDTitle1"></p>
+        <div id="H">
+          <h1 id="toc0">1.&#xA0; Clause</h1>
+          <p>Text</p>
+        </div>
+      </main>
     OUTPUT
 
     processor.output(input, "test.xml", "test.html", :html)
     expect(File.exists?("test.html")).to be true
     expect(
-      xmlpp(File.read("test.html", encoding: "utf-8").gsub(%r{^.*<main}m, "<main").gsub(%r{</main>.*}m, "</main>"))
+      xmlpp(File.read("test.html", encoding: "utf-8")
+        .gsub(%r{^.*<main}m, "<main")
+        .gsub(%r{</main>.*}m, "</main>"))
     ).to be_equivalent_to xmlpp(output)
-
   end
 
   it "generates DOC from IsoDoc XML" do
-    FileUtils.rm_f "test.xml"
-    FileUtils.rm_f "test.doc"
     input = <<~"INPUT"
-    <mpfd-standard xmlns="http://riboseinc.com/isoxml">
-      <sections>
-        <clause id="H" obligation="normative"><title>1.<tab/>Clause</title>
-          <p>Text</p>
-        </clause>
-      </sections>
-    </mpfd-standard>
+      <mpfd-standard xmlns="http://riboseinc.com/isoxml">
+        <sections>
+          <clause id="H" obligation="normative"><title>1.<tab/>Clause</title>
+            <p>Text</p>
+          </clause>
+        </sections>
+      </mpfd-standard>
     INPUT
 
     output = <<~"OUTPUT"
-    <div class="WordSection3">
-  <p class="zzSTDTitle1"></p>
-  <div><a name="H" id="H"></a>
-    <h1>1.<span style="mso-tab-count:1">&#xA0; </span>Clause</h1>
-    <p class="MsoNormal">Text</p>
-  </div>
-</div>
+      <div class="WordSection3">
+        <p class="zzSTDTitle1"></p>
+        <div><a name="H" id="H"></a>
+          <h1>1.<span style="mso-tab-count:1">&#xA0; </span>Clause</h1>
+          <p class="MsoNormal">Text</p>
+        </div>
+      </div>
     OUTPUT
 
     processor.output(input, "test.xml", "test.doc", :doc)
-   expect(File.exists?("test.doc")).to be true
+    expect(File.exists?("test.doc")).to be true
 
-   expect(
-      xmlpp(File.read("test.doc", encoding: "utf-8").gsub(%r{^.*<div class="WordSection3"}m, %(<div class="WordSection3")).gsub(%r{<div style="mso-element:footnote-list".*}m, ""))
+    expect(
+      xmlpp(File.read("test.doc", encoding: "utf-8")
+        .gsub(%r{^.*<div class="WordSection3"}m, %(<div class="WordSection3"))
+        .gsub(%r{<div style="mso-element:footnote-list".*}m, ""))
     ).to be_equivalent_to xmlpp(output)
-
   end
 end
