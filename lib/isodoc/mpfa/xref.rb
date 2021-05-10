23d0d19e9716bc84fea1196fc8492a5a3c275923
Nick Nicholas <opoudjis@gmail.com>
2021-05-10 20:36:18 +1000

    version bump

--- lib/isodoc/mpfa/xref.rb
+++ lib/isodoc/mpfa/xref.rb
@@ -3,18 +3,18 @@ module IsoDoc
     class Xref < IsoDoc::Xref
       FRONT_CLAUSE = "//*[parent::preface]".freeze
 
-      def initial_anchor_names(d)
-        d.xpath(ns(self.class::FRONT_CLAUSE)).each do |c|
+      def initial_anchor_names(doc)
+        doc.xpath(ns(self.class::FRONT_CLAUSE)).each do |c|
           preface_names(c)
           sequential_asset_names(c)
         end
-        middle_section_asset_names(d)
-        clause_names(d, 0)
-        termnote_anchor_names(d)
-        termexample_anchor_names(d)
+        middle_section_asset_names(doc)
+        clause_names(doc, 0)
+        termnote_anchor_names(doc)
+        termexample_anchor_names(doc)
       end
 
-      def annex_name_lbl(clause, num)
+      def annex_name_lbl(_clause, num)
         l10n("<strong>#{@labels['annex']} #{num}</strong>")
       end
 
@@ -31,20 +31,20 @@ module IsoDoc
       def container_names(clause, lvl)
         if clause["container"]
           @anchors[clause["id"]] =
-            { label: nil, xref: clause.at(ns("./title"))&.text, level: lvl+1 }
+            { label: nil, xref: clause.at(ns("./title"))&.text, level: lvl + 1 }
         end
         clause.xpath(ns("./clause | ./term  | ./terms | "\
                         "./definitions")).each do |c|
-          container_names(c, clause["container"] ? lvl+1 : lvl)
+          container_names(c, clause["container"] ? lvl + 1 : lvl)
         end
       end
 
-      def sect_names(clause, num, i, lvl, prev_lvl)
+      def sect_names(clause, num, i, lvl, _prev_lvl)
         return i if clause.nil?
+
         curr = i
         if !clause["container"]
-          retlvl = lvl
-          i+=1
+          i += 1
           curr = i
           name = num.nil? ? i.to_s : "#{num}.#{i}"
           @anchors[clause["id"]] = { label: name, level: lvl + 1,
@@ -57,28 +57,29 @@ module IsoDoc
           if clause["container"]
             i, lvl = sect_names(c, num, i, lvl, lvl)
           else
-            j, prev = sect_names(c, name, j, lvl+1, prev)
+            j, prev = sect_names(c, name, j, lvl + 1, prev)
           end
         end
-        i = j if j >0
+        i = j if j.positive?
         i = curr if lvl < prev
         [i, prev]
       end
 
-      def annex_naming(c, num, lvl, i)
-        if c["guidance"] then annex_names1(c, "#{num}E", lvl + 1)
+      def annex_naming(clause, num, lvl, idx)
+        if clause["guidance"] then annex_names1(clause, "#{num}E", lvl + 1)
         else
-          i+= 1 unless c["container"]
-          annex_names1(c, "#{num}.#{i}", lvl + 1)
+          idx += 1 unless clause["container"]
+          annex_names1(clause, "#{num}.#{idx}", lvl + 1)
         end
-        i
+        idx
       end
 
       def annex_names(clause, num)
-        @anchors[clause["id"]] = { label: annex_name_lbl(clause, num), value: num,
-                                   xref: l10n("#{@labels['annex']} #{num}"), level: 1 }
+        @anchors[clause["id"]] =
+          { label: annex_name_lbl(clause, num), value: num,
+            xref: l10n("#{@labels['annex']} #{num}"), level: 1 }
         if a = single_annex_special_section(clause)
-          annex_names1(a, "#{num}", 1)
+          annex_names1(a, num.to_s, 1)
         else
           i = 0
           clause.xpath(ns("./clause | ./references")).each do |c|
@@ -90,8 +91,11 @@ module IsoDoc
       end
 
       def annex_names1(clause, num, level)
-        clause["container"] or @anchors[clause["id"]] =
-          { label: num, xref: l10n("#{@labels['annex']} #{num}"), level: level }
+        unless clause["container"]
+          @anchors[clause["id"]] =
+            { label: num,
+              xref: l10n("#{@labels['annex']} #{num}"), level: level }
+        end
         i = 0
         clause.xpath(ns("./clause | ./references")).each do |c|
           i = annex_naming(c, num, level, i)
