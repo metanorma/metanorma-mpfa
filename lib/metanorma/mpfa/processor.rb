d8d169ea28002400921b5e8f4ab6a62a93e23697
Alexander Dyuzhev <dyuzhev@gmail.com>
2023-06-16 18:57:25 +0300

    Noto fonts added, metanorma/mn2pdf#200

--- lib/metanorma/mpfa/processor.rb
+++ lib/metanorma/mpfa/processor.rb
@@ -23,6 +23,18 @@ module Metanorma
           "Arial" => nil,
           "Source Han Sans" => nil,
           "STIX Two Math" => nil,
+          "Noto Sans" => nil,
+          "Noto Sans HK" => nil,
+          "Noto Sans JP" => nil,
+          "Noto Sans KR" => nil,
+          "Noto Sans SC" => nil,
+          "Noto Sans TC" => nil,
+          "Noto Sans Mono" => nil,
+          "Noto Sans Mono CJK HK" => nil,
+          "Noto Sans Mono CJK JP" => nil,
+          "Noto Sans Mono CJK KR" => nil,
+          "Noto Sans Mono CJK SC" => nil,
+          "Noto Sans Mono CJK TC" => nil,
         }
       end
 
